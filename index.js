757fbc90538036d7ec2fcc192e954acf329cadf8
James Aules <james_aules@icloud.com>
2022-09-22 19:59:41 -0400

    adding queyselector

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll(".dog-image")
+ for (let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+    
+ }
+
+ let Dognames = document.querySelectorAll(".dog-name")
+for (let i = 0; i < Dognames.length; i++) {
+        Dognames[i].style.textAlign = "left"
+        
+    }
